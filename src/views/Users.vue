2ac7735b70cc9c50ef7e440a9864ccfc8fcd5869
yokotadaiki <yd43star@gmail.com>
2020-07-15 01:26:54 +0900

    second commit

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -3,6 +3,7 @@
     <h3>Users</h3>
     <router-link to="/users/1">ユーザー1</router-link>
     <router-link to="/users/2">ユーザー2</router-link>
+    <router-link to="/users/2">ユーザー3</router-link>
     <hr>
     <h1>User No. {{ $route.params.id }}</h1>
   </div>
